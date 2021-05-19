9852b8c10e158793da9451379cacfe5b8fe4af82
wangjinbiao1985 <42571215+wangjinbiao1985@users.noreply.github.com>
2021-05-19 14:40:54 +0800

    done

--- lib/flutter_reboot.dart
+++ lib/flutter_reboot.dart
@@ -7,8 +7,21 @@ class FlutterReboot {
   static const MethodChannel _channel =
       const MethodChannel('flutter_reboot');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  // static Future<String?> get platformVersion async {
+  //   final String? version = await _channel.invokeMethod('getPlatformVersion');
+  //   return version;
+  // }
+
+  static Future<void> get rebootApp async {
+    await _channel.invokeMethod('reboot_app');
+  }
+
+  static Future<void> get rebootSystem async {
+    await _channel.invokeMethod('reboot_system');
+  }
+
+  static Future<bool?> get isRooted async {
+    final bool? root = await _channel.invokeMethod('is_rooted');
+    return root;
   }
 }
