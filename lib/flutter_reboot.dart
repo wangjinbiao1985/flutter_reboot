cad7eaec84d01f3e7b91afd6d7500820fc2836ef
wangjinbiao1985 <42571215+wangjinbiao1985@users.noreply.github.com>
2021-06-03 11:54:11 +0800

    Update flutter_reboot.dart

--- lib/flutter_reboot.dart
+++ lib/flutter_reboot.dart
@@ -1,12 +1,10 @@
-
 import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
 
 class FlutterReboot {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_reboot');
+  static const MethodChannel _channel = const MethodChannel('flutter_reboot');
 
   // static Future<String?> get platformVersion async {
   //   final String? version = await _channel.invokeMethod('getPlatformVersion');
